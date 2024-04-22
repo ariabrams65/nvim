6b08def1099bca23cf4ac6befa0797f4a1c4f850
ariabrams65 <ariabrams65>
2024-04-22 13:40:50 -0700

    More refactoring

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -23,3 +23,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set('n', '<A-,>', '<cmd>bnext<CR>')
+vim.keymap.set('n', '<A-.>', '<cmd>bprevious<CR>')
