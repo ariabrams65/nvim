ad0d9fae1a191e8ea809a39227eda402e26cae70
ariabrams65 <ariabrams65>
2024-04-22 01:14:06 -0700

    Changes

--- init.lua
+++ init.lua
@@ -19,15 +19,31 @@ vim.opt.cursorline = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.keymap.set('i', 'kj', '<Esc>')
+vim.opt.signcolumn = 'yes'
 
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '<A-d>', '<C-d>zz')
+vim.keymap.set('n', '<A-u>', '<C-u>zz')
+
+vim.keymap.set('n', '<A-i>', '<C-i>')
+vim.keymap.set('n', '<A-o>', '<C-o>')
+
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
 
 vim.keymap.set('n', '<A-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<A-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<A-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<A-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set('n', '<leader>vc', '<cmd>e ~/.config/nvim/init.lua<CR>')
 
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
@@ -45,23 +61,47 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
+    {
+        'eandrju/cellular-automaton.nvim',
+        config = function()
+            vim.keymap.set('n', '<leader>mr', '<cmd>CellularAutomaton make_it_rain<CR>')
+        end
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+        config = function()
+            vim.keymap.set('n', '<leader>md', ':MarkdownPreview<CR>', {})
+        end
+    },
     {'numToStr/Comment.nvim', opts = {}},
     {
         'lukas-reineke/indent-blankline.nvim',
         main = 'ibl',
         config = function()
             require('ibl').setup({
-                scope = {enabled = false}
+                scope = {enabled = false},
+                exclude = {filetypes = {'text', 'markdown'}}
             })
         end
     },
     {
         'neanias/everforest-nvim',
-        version = false, 
-        lazy = false, 
+        version = false,
+        lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme 'everforest'
+            -- vim.cmd.colorscheme 'everforest'
+        end
+    },
+    {
+        "oxfist/night-owl.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("night-owl")
         end
     },
     {
@@ -69,23 +109,30 @@ require('lazy').setup({
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require('telescope.builtin')
+            local utils = require('telescope.utils')
             vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>sr', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
             vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>ff', builtin.current_buffer_fuzzy_find, {})
+            vim.keymap.set('n', '<leader>sd', function()
+                builtin.find_files({cwd = utils.buffer_dir()})
+            end,{})
         end
     },
     {
-        "nvim-treesitter/nvim-treesitter", 
+        "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function() 
+        config = function()
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = {'lua', 'python', 'vimdoc'},
+                --automatically installs parser when encountering that filetype
+                auto_install = true,
+                -- ensure_installed = {'lua', 'python', 'vimdoc', 'javascript', 'json'},
                 highlight = { enable = true },
-                indent = { enable = true },  
+                indent = { enable = true },
             })
         end
     },
@@ -94,7 +141,7 @@ require('lazy').setup({
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
             require('lualine').setup({
-                options = {theme = 'everforest'}
+                options = {theme = 'auto'}
             })
         end
     },
@@ -102,5 +149,32 @@ require('lazy').setup({
         'windwp/nvim-autopairs',
         event = 'InsertEnter',
         config = true
+    },
+    {
+        'williamboman/mason.nvim',
+        config = function()
+            require('mason').setup()
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        config = function()
+            require('mason-lspconfig').setup({
+                ensure_installed = {'lua_ls', 'tsserver'}
+            })
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        config = function()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({})
+            lspconfig.tsserver.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+        end
     }
 }, {})
+
